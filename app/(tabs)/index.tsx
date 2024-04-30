6a494d92a955f30735eefab6f5cc175657cc60e7
d0ugui <douglaspo_97@outlook.com>
2024-04-30 17:54:24 -0300

    feat: create home page and add images to assets

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,16 @@
-import React from "react";
-import { Text, View } from "react-native";
+import { router } from "expo-router";
+import { Text, TouchableOpacity, View } from "react-native";
 
 export default function TabOneScreen() {
   return (
     <View>
       <Text>Olá, tabs</Text>
+      <TouchableOpacity
+        className="w-full rounded-lg bg-[#FF0000]"
+        onPress={() => router.push("/")}
+      >
+        <Text>Let's Go</Text>
+      </TouchableOpacity>
     </View>
   );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,43 @@
+import { router } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { Text } from "react-native";
+import { Image, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function App() {
   return (
-    <SafeAreaView>
-      <Text className="font-pbold text-3xl">Ola</Text>
+    <SafeAreaView className="bg-[#1C1833]">
+      <View className="px-8 py-8 flex-col items-center justify-center h-full relative">
+        <Image
+          source={require("../assets/images/shield.png")}
+          className="w-60 h-60"
+          resizeMode="contain"
+        />
+
+        <Image
+          source={require("../assets/images/marvel-logo.png")}
+          className="w-full"
+          resizeMode="cover"
+        />
+
+        <Text className="text-white font-psemibold text-2xl mt-5 text-center">
+          Sua enciclopédia {"\n"}
+          do universo marvel
+        </Text>
+
+        <Text className="text-white font-pregular mt-5 text-lg">
+          Encontre seus heróis favoritos
+        </Text>
+
+        <TouchableOpacity
+          className="w-full rounded-lg bg-[#FF0000] h-14 items-center justify-center mt-5"
+          onPress={() => router.push("/(tabs)")}
+        >
+          <Text className="font-pbold text-2xl text-white">Vamos lá</Text>
+        </TouchableOpacity>
+      </View>
+
+      <StatusBar backgroundColor="bg-[#1C1833]" style="light" />
     </SafeAreaView>
   );
 }
